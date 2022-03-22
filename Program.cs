d28dda8bc81ad73df703382ebaeb0752612ca15e
DmitryElis <99483929+DmitryElis@users.noreply.github.com>
2022-03-22 12:07:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            bool himgeenias;
+            char gender;
+            byte age;
+            float growth;
+            double weight;
+            decimal harism;
+            int iQ;
+            sbyte longlife;
+            short workhour;
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
