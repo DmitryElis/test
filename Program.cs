de7c4c4e06c5db3998be43564fd0beeec2d7bf74
DmitryElis <99483929+DmitryElis@users.noreply.github.com>
2022-03-22 13:19:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,35 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test4
+namespace test_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string name;
-            string age;
-            string zodiak;
-            string prof;
-
-            Console.WriteLine("Введите ваше имя :");
-            name = Console.ReadLine();
-
-            Console.WriteLine("Введите ваш возраст :");
-            age = Console.ReadLine();
-
-            Console.WriteLine("Введите ваш знак зодиака :");
-            zodiak = Console.ReadLine();
-            
-            Console.WriteLine("Введите вашу профессию ");
-            prof = Console.ReadLine(); 
-
-            Console.BackgroundColor = ConsoleColor.Green;  
-            Console.ForegroundColor = ConsoleColor.Black;    
-            
-            Console.WriteLine("Ваше имя " + name + ".\nВам " + age + "лет.\nВаш знак зодиака " + zodiak + ".\nРаботаете по профессии " + prof + ".");
-
-            
         }
     }
 }
